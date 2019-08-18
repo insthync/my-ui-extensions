b79dc048a67df023ffc92e85bc51aab7836bd8ba
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-18 13:53:35 +0700

    Add scale ui extension

---
+++ Scripts/ScaleUIExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ScaleUIExtension : MonoBehaviour, IUIExtension
+{
+    public float showDuration = 0.5f;
+    public Ease showEase = Ease.OutBack;
+    public float hideDuration = 0.5f;
+    public Ease hideEase = Ease.InBack;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        transform.DOScale(Vector3.one, showDuration).SetEase(showEase);
+    }
+
+    public void Hide()
+    {
+        transform.DOScale(Vector3.zero, hideDuration).SetEase(hideEase).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
+    }
+}
