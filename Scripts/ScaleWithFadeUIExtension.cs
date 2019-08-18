45d5f68a1f090751327b917ed59f78c2ba2234fb
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-08-18 21:41:59 +0700

    Add scale with fade ui extension

---
+++ Scripts/ScaleWithFadeUIExtension.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class ScaleWithFadeUIExtension : MonoBehaviour, IUIExtension
+{
+    public Image fadeImage;
+    public float showFadeDuration = 0.5f;
+    public float showFadeAlpha = 0.5f;
+    public float hideFadeDuration = 0.5f;
+    public float hideFadeAlpha = 0;
+    public Transform scaleTransform;
+    public float showScaleDuration = 0.5f;
+    public Ease showEase = Ease.OutBack;
+    public float hideScaleDuration = 0.5f;
+    public Ease hideEase = Ease.InBack;
+
+    public void Show()
+    {
+        gameObject.SetActive(true);
+        scaleTransform.localScale = Vector3.zero;
+        scaleTransform.DOScale(Vector3.one, showScaleDuration).SetEase(showEase);
+        fadeImage.color = new Color(fadeImage.color.r, fadeImage.color.g, fadeImage.color.b, hideFadeAlpha);
+        fadeImage.DOFade(showFadeAlpha, showFadeDuration);
+    }
+
+    public void Hide()
+    {
+        scaleTransform.localScale = Vector3.one;
+        scaleTransform.DOScale(Vector3.zero, hideScaleDuration).SetEase(hideEase).OnComplete(() =>
+        {
+            gameObject.SetActive(false);
+        });
+        fadeImage.DOFade(hideFadeAlpha, hideFadeDuration);
+    }
+}
